396df6c66e1d886306f03344c645691c2704cfcb
nemwolf <nemwolf@proton.me>
2026-08-17 07:18:51 -0500

    added umbreon image

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 document.getElementById('year').textContent = new Date().getFullYear();
 
+//will unhide element
+//TODO: make this function take in any element, not just this
 function unHidePlush()
-{
+    {
     
-    if (document.getElementById('plush_image').style.display==="block")
-    {
-        document.getElementById('plush_image').style.display="none"
+        if (document.getElementById('plush_image').style.display==="block")
+            {
+                document.getElementById('plush_image').style.display="none"
+            }
+        else
+            {
+                document.getElementById('plush_image').style.display="block";
+            }
     }
-    else
-    {
-        document.getElementById('plush_image').style.display="block";
-    }
-}
