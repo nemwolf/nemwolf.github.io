52ca6f1b43e5602fe234cb8956da42ca47b38936
nemwolf <nemwolf@proton.me>
2026-08-18 22:11:31 -0500

    more stuff i guess

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ document.getElementById('year').textContent = new Date().getFullYear();
 
 //will unhide element
 //TODO: make this function take in any element, not just this
-function unHidePlush()
-    {
-    
-        if (document.getElementById('plush_image').style.display==="block")
-            {
-                document.getElementById('plush_image').style.display="none"
-            }
-        else
-            {
-                document.getElementById('plush_image').style.display="block";
-            }
+function unHidePlush() {
+
+    if (document.getElementById('plush_image').style.display === "block") {
+        document.getElementById('plush_image').style.display = "none"
     }
+    else {
+        document.getElementById('plush_image').style.display = "block";
+    }
+}
